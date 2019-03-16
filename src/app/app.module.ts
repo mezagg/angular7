101d8e62e2cc14f772c30263dc3ef28421b8ccfd
Gerardo Meza <mezagg@gmail.com>
2019-03-15 23:41:16 -0600

    agregar routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 
+import {routing, appRoutingProviders} from './app.routing';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RouteRoutingModule,
+    routing
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+  	appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

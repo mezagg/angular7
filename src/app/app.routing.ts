7c79e692b3b7a5aed4132c2e8f08b8e253964dd4
Gerardo Meza <mezagg@gmail.com>
2019-03-23 01:15:28 -0600

    Listado de productos

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,15 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import {HomeComponent} from './components/home/home.component'
+
+import {ListaProductosComponent} from './components/lista-productos/lista-productos.component'
 import {ErrorComponent} from './components/error/error.component'
 
 const routes: Routes = [
 {path: '', component: HomeComponent},
 {path: 'home', component: HomeComponent},
+{path: 'products', component: ListaProductosComponent},
+
 {path: '**', component: ErrorComponent}
 
 ];
